96467260f84f6b9523ec102d2c64a37d6a2405c6
bjjones8512 <bjjones8512@yahoo.com>
2025-02-03 08:24:09 -0600

    business rule

--- src/Component/Test.js
+++ src/Component/Test.js
@@ -23,3 +23,13 @@ grIssue.addQuery('item', current.getUniqueValue());
 grIssue.addQuery('opened_at', '>=', lastYearStartDate);
 grIssue.addQuery('opened_at', '<=', lastYearEndDate);
 // End STRY0984137
+
+/*I believe the requirement is not clear from the PO, added my comments on the Story.
+I have looked at the changes on the Business Rule: OneRisk_Calculate_SOX_Risk_Score_Control and it shouldn't be in that way.
+The code should be in the way at line 48 to get the query as below.
+From: Add '4' if any Issue record created for the Control in last year (i.e. 4 January 2024 to 3 January 2025).
+https://ays-dev.verizon.com/sn_grc_issue_list.do?sysparm_query=item.name%3DSTP.5.12.3%20Maintain%20System%20Settings%20%26%20Security%5Eopened_atRELATIVEGT%40year%40ago%401
+To: Add '4' if any Issue record created for the Control in last year as below.
+1. When calculation happens on Jan 15, 2025 --> March 1, 2023 and February 29, 2024
+2. When calculation happens on Sep 3, 2026 --> March 1, 2025 and February 28, 2026.
+*/
