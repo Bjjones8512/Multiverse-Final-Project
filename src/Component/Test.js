1762f956c9b4e1a96d6d8c077428864187baffdd
Bjjones8512 <bjjones8512@yahoo.com>
2025-02-03 08:32:36 -0600

    Update Test.js

--- src/Component/Test.js
+++ src/Component/Test.js
@@ -11,10 +11,9 @@
     var riskScore = 0; // Initialize the variable to 0
 
     // Fetch the Average of all completed control attestation's sox risk score related to control
-
     var grAsmt = new GlideAggregate('asmt_assessment_instance');
     grAsmt.addQuery("sn_grc_item", current.getUniqueValue());
-	grAsmt.addEncodedQuery("taken_onONThis year@javascript:gs.beginningOfThisYear()@javascript:gs.endOfThisYear()"); // Added per STRY0951832
+    grAsmt.addEncodedQuery("taken_onONThis year@javascript:gs.beginningOfThisYear()@javascript:gs.endOfThisYear()"); // Added per STRY0951832
     grAsmt.query("state", "complete");
     grAsmt.setGroup(false);
     grAsmt.addAggregate('AVG', 'u_sox_risk_score');
@@ -24,7 +23,7 @@
         riskScore = parseInt(avgRiskScore); // set the average sox risk score value to riskscore variable
     }
 
-    // Check if control's nature is either "manual" or "IT Dependent - Manual". If it is then add 2 to risk score variable	
+    // Check if control's nature is either "manual" or "IT Dependent - Manual". If it is then add 2 to risk score variable    
     if (current.u_nature == 'Manual' || current.u_nature == 'IT Dependent - Manual') {
         riskScore += 2;
     }
@@ -43,50 +42,33 @@
         }
     }
 
-    // Check if there are any issue created within last 1 year for this control. If it is then add 4
+    // STRY0984137 - Blake - Updated date range for issue records to consider March 1 of last year to February 28 (or 29 if leap year) of current year
+    var today = new GlideDateTime();
+    var currentYear = today.getYearUTC();
+    var lastYear = currentYear - 1;
+
+    // Set start date as March 1 of the previous year
+    var lastYearStartDate = new GlideDateTime(lastYear + "-03-01 00:00:00");
+
+    // Set end date as February 28 of the current year by default
+    var lastYearEndDate = new GlideDateTime(currentYear + "-02-28 23:59:59");
+
+    // Adjust end date if the current year is a leap year (i.e., February 29 exists)
+    if (new GlideDateTime(currentYear + "-02-29").getDate().indexOf("Invalid") === -1) {
+        lastYearEndDate = new GlideDateTime(currentYear + "-02-29 23:59:59");
+    }
+
+    // Query for issues opened within the corrected last-year fiscal range
     var grIssue = new GlideRecord('sn_grc_issue');
-    grIssue.addEncodedQuery('item=' + current.getUniqueValue() + '^opened_atRELATIVEGT@year@ago@1');
+    grIssue.addQuery('item', current.getUniqueValue());
+    grIssue.addQuery('opened_at', '>=', lastYearStartDate);
+    grIssue.addQuery('opened_at', '<=', lastYearEndDate);
     grIssue.query();
     if (grIssue.hasNext()) {
         riskScore += 4;
     }
 
-	// STRY0984137 - Blake - Updated date range to consider assessments taken from March 1 - January 31 of the current year
-	var today = new GlideDateTime();
-	var currentYear = today.getYearUTC(); 
-	var currentYearStartDate = new GlideDateTime(currentYear + "-03-01 00:00:00");
-	var currentYearEndDate = new GlideDateTime(currentYear + "-01-31 23:59:59");
-
-	grAsmt.addQuery("taken_on", ">=", currentYearStartDate);
-	grAsmt.addQuery("taken_on", "<=", currentYearEndDate);
-
-
-	// STRY0984137 - Blake - Updated date range for issue records to consider March 1 of last year to February 28 (or 29 if leap year) of current year
-	var lastYear = currentYear - 1;
-	var lastYearStartDate = new GlideDateTime(lastYear + "-03-01 00:00:00");
-	var lastYearEndDate = new GlideDateTime(lastYear + "-02-28 23:59:59");
-
-	// Check if last year was a leap year and update end date accordingly
-	if (new GlideDateTime(lastYear + "-02-29").getDate().indexOf("Invalid") === -1) {
-		lastYearEndDate = new GlideDateTime(lastYear + "-02-29 23:59:59");
-	}
-
-	grIssue.addQuery('item', current.getUniqueValue());
-	grIssue.addQuery('opened_at', '>=', lastYearStartDate);
-	grIssue.addQuery('opened_at', '<=', lastYearEndDate);
-
     // Update the SOX risk score field value with finalized riskscore.
     current.u_sox_risk_score = riskScore;
 
-
 })(current, previous);
-
-/*I believe the requirement is not clear from the PO, added my comments on the Story.
-I have looked at the changes on the Business Rule: OneRisk_Calculate_SOX_Risk_Score_Control and it shouldn't be in that way.
-The code should be in the way at line 48 to get the query as below.
-From: Add '4' if any Issue record created for the Control in last year (i.e. 4 January 2024 to 3 January 2025).
-https://ays-dev.verizon.com/sn_grc_issue_list.do?sysparm_query=item.name%3DSTP.5.12.3%20Maintain%20System%20Settings%20%26%20Security%5Eopened_atRELATIVEGT%40year%40ago%401
-To: Add '4' if any Issue record created for the Control in last year as below.
-1. When calculation happens on Jan 15, 2025 --> March 1, 2023 and February 29, 2024
-2. When calculation happens on Sep 3, 2026 --> March 1, 2025 and February 28, 2026.
-*/
